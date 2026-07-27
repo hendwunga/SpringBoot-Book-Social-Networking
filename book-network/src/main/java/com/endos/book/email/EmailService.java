67be0dc25df9e53f15462ea3ba0dc405054ffc90
hendrowunga <hendrowunga073@gmail.com>
2026-07-27 22:11:32 +0700

    fix(email): resolve Thymeleaf template resolution for activation emails
    - Change emailTemplate.name() (enum default) to emailTemplate.getName()
      which returns the correct template name 'activate_account'
    - Fixes activation emails failing to send due to missing template

--- book-network/src/main/java/com/endos/book/email/EmailService.java
+++ book-network/src/main/java/com/endos/book/email/EmailService.java
@@ -31,7 +31,7 @@ public class EmailService {
         if (emailTemplate == null) {
             templateName = "confirm-email";
         } else {
-            templateName = emailTemplate.name();
+            templateName = emailTemplate.getName();
         }
         MimeMessage mimeMessage = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(
